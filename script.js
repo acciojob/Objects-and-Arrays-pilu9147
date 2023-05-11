97b1c32b9f30a0937087fb4f9cdae8afe2f7ebf0
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-11 18:01:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-
+ const team = [];
+    players.forEach(function (item) {
+        team.push(item);
+    });
+  let team1 = Array.from(players)
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
+const cap1 = {
+  name: "John Doe",
+  age: 80,
+};
 window.players = players;
 window.person = person;
 window.team = team;
